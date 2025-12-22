4e06a572872b45a4bd08ffec32fe39599135e868
arvianrsl <raisulakbar777@gmail.com>
2025-12-22 15:18:20 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,8 @@ const urlsToCache = [
   `${BASE_URL}offline.html`,
   `${BASE_URL}assets/style.css`,
   `${BASE_URL}manifest.json`,
-  `${BASE_URL}assets/sijiwa192.png`,
-  `${BASE_URL}assets/sijiwa512.png`,
+  `${BASE_URL}assets/sijiwa192x192.png`,
+  `${BASE_URL}assets/sijiwa512x512.png`,
 ];
 
 // Install Service Worker & simpan file ke cache
