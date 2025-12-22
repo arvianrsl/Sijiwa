222de0b68cf1a14b363942b14c4fc82e92a162c0
arvianrsl <raisulakbar777@gmail.com>
2025-12-22 12:12:20 +0700

    icona sevicework

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,8 @@ const urlsToCache = [
   `${BASE_URL}offline.html`,
   `${BASE_URL}assets/style.css`,
   `${BASE_URL}manifest.json`,
-  `${BASE_URL}logo sijiwa 192x192.png`,
-  `${BASE_URL}logo sijiwa.png`,
+  `${BASE_URL}icons/logo-sijiwa-192.png`,
+  `${BASE_URL}icons/logo-sijiwa-512.png`,
 ];
 
 // Install Service Worker & simpan file ke cache
